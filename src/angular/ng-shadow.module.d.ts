4c508305aa2239d48acc722bbf34c95a3a0fd46a
Gabriel Rodríguez Alsina <gabitoesmiapodo@users.noreply.github.com>
2017-12-22 21:36:12 -0300

    Small fix to allow for TNS + NG + webpack builds.

--- src/angular/ng-shadow.module.d.ts
+++ src/angular/ng-shadow.module.d.ts
@@ -1,2 +1,12 @@
+// Base modules, do not touch.
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+
+@NgModule({
+    imports: [],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
+})
+
 export declare class NgShadowModule {
 }
